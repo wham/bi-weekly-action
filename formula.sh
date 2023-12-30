171dd81d6911a5071b055903c15cf7420ccdaf23
Tomáš Veselý <wham@users.noreply.github.com>
2023-12-30 03:23:26 +0000

    weekdfiff -> weekindex

--- formula.sh
+++ formula.sh
@@ -6,5 +6,5 @@ current_date="2023-12-25"
 weekdiff=$((($(date -d $current_date +%s) - $(date -d $first_run_date +%s)) / 60 / 60 / 24 / 7))
 weekindex=$((weekdiff % 2))
 
-echo $weekdiff
-echo $weekindex
+echo $weekdiff # -> 4
+echo $weekindex # -> 0
